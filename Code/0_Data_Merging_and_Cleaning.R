395aae1d653d978286a69917568ed30344b06f2e
Clare Clingain <cclingain@gmail.com>
2019-06-13 19:02:09 -0400

    initialize merge clean script

---
+++ Code/0_Data_Merging_and_Cleaning.R
@@ -0,0 +1,8 @@
+##################################
+# 0 - Data Merging and Cleaning
+##################################
+
+
+# Import 2011 Census Data
+family_2011 <- read.csv("Data/family_2011_census.csv")
+commuting_2011 <- read.csv("Data/commuting_2011_census.csv")
