ab1f379b44204bbbf155a8164d787417d5c1059a
Clare Clingain <cclingain@gmail.com>
2019-06-13 19:19:38 -0400

    added family data from 2016 census

--- Code/0_Data_Merging_and_Cleaning.R
+++ Code/0_Data_Merging_and_Cleaning.R
@@ -6,3 +6,11 @@
 # Import 2011 Census Data
 family_2011 <- read.csv("Data/family_2011_census.csv")
 commuting_2011 <- read.csv("Data/commuting_2011_census.csv")
+
+# Import 2016 Census Data
+family_2016 <- read.csv("Data/family_2016_census.csv")
+
+# Equivalents
+#      2011                   --->    2016
+# Electoral.Division.CSO.Code --->   CSOED
+# SA_Name                     --->   OSIED
