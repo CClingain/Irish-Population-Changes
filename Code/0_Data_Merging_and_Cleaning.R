86651dac527e591f449539351a151b9e0cdb4709
Clare Clingain <cclingain@gmail.com>
2019-06-14 13:10:12 -0400

    fix merge and remove extra variables

--- Code/0_Data_Merging_and_Cleaning.R
+++ Code/0_Data_Merging_and_Cleaning.R
@@ -15,15 +15,14 @@ family_2016 <- read.csv("Data/family_2016_census.csv")
 # Electoral.Division.CSO.Code --->   CSOED
 # SA_Name                     --->   SA_PUB2011
 
-# Add the year to the data before merge
-family_2011$year <- 2011
-family_2016$year <- 2016
-
 # Extract variables needed
-family_2011_sub <- family_2011[,c(1:42, 110)]
-family_2016_sub <- family_2016[,c(1:21, 30:50)]
+family_2011_sub <- family_2011[,c(1:42)]
+family_2016_sub <- family_2016[,c(1:21, 30:49)]
 
 # Merge
 family <- merge(family_2011_sub, family_2016_sub, 
-                by.x = c("Electoral.Division.CSO.Code", "GEOGID"),
-                by.y = c("CSOED","GEOGID"))
+                by.x = c("Electoral.Division.CSO.Code", "GEOGID", "County", "NUTS_III"),
+                by.y = c("CSOED","GEOGID", "COUNTYNAME", "NUTS3NAME"))
+
+# Remove extra info
+family <- family[,-c(43, 45:49, 51, 53, 55:56)]
