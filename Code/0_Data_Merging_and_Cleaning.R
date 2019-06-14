d388c2660f418a23ae708259d35a1753985a1210
Clare Clingain <cclingain@gmail.com>
2019-06-14 12:39:59 -0400

    completed initial merge and added more 2016 data

--- Code/0_Data_Merging_and_Cleaning.R
+++ Code/0_Data_Merging_and_Cleaning.R
@@ -13,4 +13,17 @@ family_2016 <- read.csv("Data/family_2016_census.csv")
 # Equivalents
 #      2011                   --->    2016
 # Electoral.Division.CSO.Code --->   CSOED
-# SA_Name                     --->   OSIED
+# SA_Name                     --->   SA_PUB2011
+
+# Add the year to the data before merge
+family_2011$year <- 2011
+family_2016$year <- 2016
+
+# Extract variables needed
+family_2011_sub <- family_2011[,c(1:42, 110)]
+family_2016_sub <- family_2016[,c(1:21, 30:50)]
+
+# Merge
+family <- merge(family_2011_sub, family_2016_sub, 
+                by.x = c("Electoral.Division.CSO.Code", "GEOGID"),
+                by.y = c("CSOED","GEOGID"))
